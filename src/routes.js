5249872b90ed127686450bac4366d4ded92f45ac
PatrickMendC <patrick.navi_igb@hotmail.com>
2019-08-25 19:23:15 -0400

    criando loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'World' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Diego Fernandes',
+    email: 'diego@email.com.br',
+    password_hash: '123456',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
