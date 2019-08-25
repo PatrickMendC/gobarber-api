8fa2b90c9d0214d6933d94f05e8f35e4a94f06c4
PatrickMendC <patrick.navi_igb@hotmail.com>
2019-08-25 09:12:43 -0400

    style: formatted files on save with eslint

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,7 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "World" });
-});
+routes.get('/', (req, res) => res.json({ message: 'World' }));
 
 export default routes;
